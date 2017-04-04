1dce0bc70ffce239d551899c9a59adf71527abb3
Ajay David <antonio.g.david@gmail.com>
2017-04-04 09:27:35 -0400

    create and render React elements

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 //Code React element here
-const meInReact =
-  React.createElement('div', {className: 'me'},
-    React.createElement('h1', {}, 'An Awesome Person'),
-    React.createElement('p', {}, 'Who is learning React'),
-    React.createElement('ul', {className: 'me__interests'},
-    [
-        React.createElement('li', {}, 'JavaScript'),
-        React.createElement('li', {}, 'React'),
-        React.createElement('li', {}, 'Movies'),
-        React.createElement('li', {}, 'Ice cream')
-    ]
-  ));
 
-ReactDOM.render(list, document.getElementById('main'));
+const title = React.createElement('h1', {}, 'An Awesome Person');
+const paragraph = React.createElement('p', {}, 'Who is learning React');
+
+const javascript = React.createElement('li', {}, 'JavaScript');
+const react = React.createElement('li', {}, 'React');
+const movies = React.createElement('li', {}, 'Movies');
+const ice_cream = React.createElement('li', {}, 'Ice cream');
+const list = React.createElement('ul', {className: 'me__interests'}, [javascript, react, movies, ice_cream]);
+
+const container = React.createElement('div', {className: 'me'}, [title, paragraph, list]);
+
+ReactDOM.render(container, document.getElementById('main'));
